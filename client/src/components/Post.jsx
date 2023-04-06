e868d38f3073d754e1b047753e700fd790310f02
raquellchavezz <raquel.chavez41313@gmail.com>
2023-04-06 11:21:27 -0700

    adds a space between name and title of article

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -18,7 +18,7 @@ const Post = ({blogPost, toUpdate, toDelete}) => {
     return (
         <Card fluid>
             <Card.Content>
-            <Card.Header>{blogPost.firstname} {blogPost.lastname}{blogPost.title} {blogPost.date} {blogPost.content}</Card.Header>
+            <Card.Header>{blogPost.firstname} {blogPost.lastname} {blogPost.title} {blogPost.date} {blogPost.content}</Card.Header>
             <Button onClick={()=>{onDelete(blogPost)}} style={{padding: '0.6em', marginRight:'0.9em'}}><Icon name='trash'/></Button>
             <Button onClick={()=>{onUpdate(blogPost)}} style={{padding: '0.6em'}}> <Icon name='edit'/></Button>
             </Card.Content>
