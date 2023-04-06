1d2f809285d4c6ded8378a5797076d6b29a7b4a0
raquellchavezz <raquel.chavez41313@gmail.com>
2023-04-05 19:31:23 -0700

    fixes put and delete route in backend

--- server/server.js
+++ server/server.js
@@ -31,13 +31,13 @@ app.post("/api/addpost", async (req, res) => {
       id_post: req.body.id_post,
       firstname: req.body.firstname,
       lastname: req.body.lastname,
-      title: req.body.lastname,
-      date: req.body.date,
+      title: req.body.title,
+      date: req.body.date, //not sure about date
       content: req.body.content,
     };
     //console.log([newStudent.firstname, newStudent.lastname, newStudent.iscurrent]);
     const result = await db.query(
-      "INSERT INTO blog_posts(firstname,lastname, title, content) VALUES($1, $2, $3, $4) RETURNING *",
+      "INSERT INTO blog_posts(firstname,lastname, title, content, date) VALUES($1, $2, $3, $4, NOW()) RETURNING *",
       [
         newBlogPost.firstname,
         newBlogPost.lastname,
@@ -54,11 +54,15 @@ app.post("/api/addpost", async (req, res) => {
 });
 
 // delete request for students
-app.delete("/api/students/:blogPostId", async (req, res) => {
+app.delete("/api/deletepost/:blogPostId", async (req, res) => {
   try {
-    const studentId = req.params.studentId;
-    await db.query("DELETE FROM students WHERE id=$1", [studentId]);
-    console.log("From the delete request-url", studentId);
+    const blogPostId = req.params.blogPostId; //this is being controlled from both front and backend
+    //paraams request come from endpoint defintion
+    //req.params is an obj has key of blogpostid
+
+    //body of req is whats being sent from front end using fetch calls
+    await db.query("DELETE FROM blog_posts WHERE id_post=$1", [blogPostId]);
+    console.log("From the delete request-url", blogPostId);
     res.status(200).end();
   } catch (e) {
     console.log(e);
@@ -67,30 +71,36 @@ app.delete("/api/students/:blogPostId", async (req, res) => {
 });
 
 //A put request - Update a student
-app.put("/api/students/:studentId", async (req, res) => {
+app.put("/api/updateblog/:blogPostId", async (req, res) => {
   //console.log(req.params);
   //This will be the id that I want to find in the DB - the student to be updated
-  const studentId = req.params.studentId;
-  const updatedStudent = {
-    id: req.body.id,
+  const blogPostId = req.params.blogPostId;
+  console.log("blogPostId", blogPostId);
+  console.log("req.params obj", req.params);
+  const updatedBlogPost = {
+    id: req.body.id_post,
+    title: req.body.title,
     firstname: req.body.firstname,
     lastname: req.body.lastname,
-    iscurrent: req.body.is_current,
+    content: req.body.content,
   };
-  console.log("In the server from the url - the student id", studentId);
+  console.log("In the server from the url - the student id", blogPostId);
   console.log(
     "In the server, from the react - the student to be edited",
-    updatedStudent
+    updatedBlogPost
   );
   // UPDATE students SET lastname = "something" WHERE id="16";
-  const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
+  const query = `UPDATE blog_posts SET firstname=$1, lastname=$2, title=$3, content=$4 WHERE id_post=${blogPostId} RETURNING *`;
   const values = [
-    updatedStudent.firstname,
-    updatedStudent.lastname,
-    updatedStudent.iscurrent,
+    updatedBlogPost.firstname,
+    updatedBlogPost.lastname,
+    updatedBlogPost.title,
+    updatedBlogPost.content,
   ];
   try {
     const updated = await db.query(query, values);
+    console.log(query);
+    console.log(values);
     console.log(updated.rows[0]);
     res.send(updated.rows[0]);
   } catch (e) {
